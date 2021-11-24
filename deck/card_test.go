27595ee37f7cf57f840657ebdda1a47d6840408e
Omar Ashraf <omarashrf15@gmail.com>
2021-11-24 19:02:41 +0200

    Added Filter and sort to deck

--- deck/card_test.go
+++ deck/card_test.go
@@ -34,3 +34,37 @@ func TestDefaultSort(t *testing.T) {
 		t.Error("First card should be ace of spade")
 	}
 }
+
+func TestJokers(t *testing.T) {
+	var num int = 3
+	cards := New(Jokers(num))
+	count := 0
+	for _, c := range cards {
+		if c.Suit == Joker {
+			count++
+		}
+	}
+	if count != num {
+		t.Errorf("Expected to have %d Jokers", num)
+	}
+}
+
+func TestFilter(t *testing.T) {
+	filter := func(card Card) bool {
+		return card.Rank == Two || card.Rank == Three
+	}
+	cards := New(Filter(filter))
+	for _, c := range cards {
+		if c.Rank == Two || c.Rank == Three {
+			t.Error("We expected to not find a Two or Three")
+			return
+		}
+	}
+}
+
+func testDeck(t *testing.T) {
+	cards := New(Deck(3))
+	if len(cards) != 13*4*3 {
+		t.Error("Error wrong numbero f cards")
+	}
+}
