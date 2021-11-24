7c6919840c81d0690874ea4ab58d0721c72a1f02
Omar Ashraf <omarashrf15@gmail.com>
2021-11-24 16:23:04 +0200

    Added sorting

---
+++ card_test.go
@@ -0,0 +1,36 @@
+package deck
+
+import (
+	"fmt"
+	"testing"
+)
+
+func ExampleCard() {
+	fmt.Println(Card{Rank: Ace, Suit: Heart})
+	fmt.Println(Card{Rank: Two, Suit: Spade})
+	fmt.Println(Card{Rank: Nine, Suit: Diamond})
+	fmt.Println(Card{Rank: Ace, Suit: Club})
+	fmt.Println(Card{Suit: Joker})
+
+	// Output:
+	// Ace of Hearts
+	// Two of Spades
+	// Nine of Diamonds
+	// Ace of Clubs
+	// Joker
+}
+
+func TestNew(t *testing.T) {
+	cards := New()
+	if len(cards) != 13*4 {
+		t.Error("Wrong number of cards")
+	}
+}
+
+func TestDefaultSort(t *testing.T) {
+	cards := New(DefaultSort)
+	exp := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != exp {
+		t.Error("First card should be ace of spade")
+	}
+}
